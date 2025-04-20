62657db024230437270dad92e8ea858e7ae97da5
emmy649 <em.damyanova@gmail.com>
2025-04-20 17:31:46 +0300

    Обновени стилове, адаптивност и модали

--- vite.config.js
+++ vite.config.js
@@ -3,29 +3,31 @@ import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
+  base: '/my-shopping-app/',
   plugins: [
     react(),
     VitePWA({
       registerType: 'autoUpdate',
       includeAssets: ['icon-192.png', 'icon-512.png'],
+      manifestFilename: 'manifest.json',
       manifest: {
         name: 'Моят план',
         short_name: 'План',
-        description: 'Лично приложение за график, бележки и задачи',
-        start_url: '/',
+        start_url: '/my-shopping-app/',
+        scope: '/my-shopping-app/',
         display: 'standalone',
         background_color: '#ffffff',
-        theme_color: '#ffffff',
+        theme_color: '#a1e3b1',
         icons: [
           {
             src: '/icon-192.png',
             sizes: '192x192',
-            type: 'image/png'
+            type: 'image/png',
           },
           {
             src: '/icon-512.png',
             sizes: '512x512',
-            type: 'image/png'
+            type: 'image/png',
           }
         ]
       }
